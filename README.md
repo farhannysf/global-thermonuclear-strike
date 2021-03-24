b8cfabe99506c14b337abaaa6771297ed4b22e36
Farhan Nugroho <farhannyusuf@gmail.com>
2021-03-24 14:27:45 -0700

    Initial app commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Global Thermonuclear Strike (Docs still WIP)
+WIP 3D GIS simulation of UGM-133 Trident II D-5 ballistic missile MIRV payload detonation using Cesium in the very early state by Farhan Yusuf Nugroho
+
+# Background
+
+This is my debut JavaScript web app and I aim to educate the public with this web app about the magnitude of a nuclear weapon effect, specifically from UGM-133 Trident II D-5 ballistic missile. 
+
+This is a Free and Open Source Software made in Indonesia as a gift to the Great Britain and the world in response of Great Britain's shift in its foreign policy and national security approach to increase cap on nuclear warheads from 180 to 260 for Global Britain in a Competitive Age. 
+
+# Features
+At the moment, this simulation only models:
+
+* Static modelling of a single Mk-4 RV with 100 kT W76 mod 0 (W76-0) warhead detonation out of 8 possible RVs in MIRV payload of one UGM-133 Trident II D-5 ballistic missile and without multiple configuration possibilities of a warhead (variable yield)
+
+* Fixed detonation altitude, airburst at 750m AGL
+
+* Different overpressure levels to range of 5 overlapping radii with total fatal casualties estimation within the lethal range of nuclear explosion using Weighted Centroid geographic retrieval methodology from ArcGIS GeoEnrichment service to aggregate census data
+
+* Mushroom cloud modelling
+
+* No other effects, such as thermal damage or fallout levels are included yet
+
+* From the current features listed above, it is obvious that there are so many features still WIP. This is a bare MVP that is very primitive and I am looking for open collaboration for much more accurate modelling, more dynamic modelling taking into account temporal changes in the states of the object, as well as improved frontend design and responsiveness
+
+# How to Use
+
+* Right click on map to detonate the warhead
+* Left click on the resulting radii, mushroom cloud to inspect its properties
+
+# References
+* The Effects of Nuclear Weapons. (1977). United States: Department of Defense.
+* [Blast Wave Effects Calculator by Jean M. Bele, Physics Dept., Laboratory for Nuclear Science, MIT](https://nuclearweaponsedproj.mit.edu/Node/104)
+* [Nuclear Fireball Calculator by Jean M. Bele, Physics Dept., Laboratory for Nuclear Science, MIT](https://nuclearweaponsedproj.mit.edu/Node/105)
+* [Graphs of Nuclear Weapons Effects by Dr. Wm. Robert Johnston](http://www.johnstonsarchive.net/nuclear/nukgr3.pdf)
+* [Nukemap by Alex Wellerstein](https://nuclearsecrecy.com/nukemap/)
---
+++ main/app.js
@@ -0,0 +1,353 @@
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNTJhMGZlYS03YzBhLTRkMDItYjdhNy1jNTY5YzYzMmYxMzEiLCJpZCI6NDYzODMsImlhdCI6MTYxNjA4MDAwMX0.mEt-nrCRpc-SR9SkguIhjLOAJkLoWDienK5kOGWZjXo';
+const apiKey = "AAPKeb55cb51637e49d3bca9d10b1254eaa6yaEpF2Mf1aXs85FffEZcBXZl_Me1REzmg_5czbl5QqSQQ7KcdlKvWOlKHnhdm4Lz"; // Referrer header restricted
+
+const authentication = new arcgisRest.ApiKey({
+  key: apiKey
+});
+
+function createreloadButton() {
+  document.getElementById("ammoCount").textContent = 0
+  var btn = document.createElement("BUTTON");
+  btn.innerHTML = "Reload Weapon";
+  btn.onclick = function() {window.location.reload()};
+  var detonateNuke = document.getElementById("detonateNuke");
+  detonateNuke.replaceWith(btn)
+}
+
+function kmtom(kilometer){
+  var meter = kilometer*1000;
+  return meter
+}
+
+var viewer = new Cesium.Viewer("cesiumContainer", {
+terrainProvider: new Cesium.ArcGISTiledElevationTerrainProvider({
+    url:
+    "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer",
+}),
+});
+viewer.scene.primitives.add(Cesium.createOsmBuildings());
+var countsClicked = 0;
+var warheadYield = 100;
+var detonationAltitude = 750
+viewer.scene.canvas.addEventListener('contextmenu', (event) => {
+if (countsClicked == 1) {
+  return;
+}
+++countsClicked;
+// console.log(countsClicked)
+event.preventDefault();
+const mousePosition  = new Cesium.Cartesian2(event.clientX, event.clientY);
+const selectedLocation = viewer.scene.pickPosition(mousePosition);
+
+setMarkerInPos(Cesium.Cartographic.fromCartesian(selectedLocation));
+}, false);
+
+function setMarkerInPos(positionCartographic){
+viewer.pickTranslucentDepth = true;
+
+ring1Radius_km = Math.pow(warheadYield,0.33)*0.24; // Blast Wave Effects Calculator by Jean M. Bele, Physics Dept., Laboratory for Nuclear Science, MIT https://nuclearweaponsedproj.mit.edu/Node/104
+// console.log(ring1Radius_km)
+ring1Radius_m = kmtom(ring1Radius_km)
+// console.log(ring1Radius_m)
+var ring1 = viewer.entities.add({
+position: Cesium.Cartesian3.fromRadians(positionCartographic.longitude, positionCartographic.latitude, 10),
+name: ring1Radius_km.toFixed(1) + " km radius",
+description:'\
+<img\
+  width="50%"\
+  style="float:left; margin: 0 1em 1em 0;"\
+  src="assets/overpressure-distance.png"/>\
+<h3>\
+  Peak overpressure level:\
+</h3>\
+<p>\
+  137.89 kPa\
+</p>\
+<h3>\
+  Estimated Blast Structural Damage:\
+</h3>\
+<p>\
+  Heavily built concrete buildings are severely damaged or demolished\
+</p>', // The Effects of Nuclear Weapons. (1977). United States: Department of Defense.
+ellipse: {
+    semiMinorAxis: ring1Radius_m,
+    semiMajorAxis: ring1Radius_m,
+    material: Cesium.Color.RED.withAlpha(0.3),
+},
+});
+
+ring2Radius_km = Math.pow(warheadYield,0.33)* 0.37; // Blast Wave Effects Calculator by Jean M. Bele, Physics Dept., Laboratory for Nuclear Science, MIT https://nuclearweaponsedproj.mit.edu/Node/104
+// console.log(ring2Radius_km)
+ring2Radius_m = kmtom(ring2Radius_km);
+// console.log(ring2Radius_m)
+var ring2 = viewer.entities.add({
+position: Cesium.Cartesian3.fromRadians(positionCartographic.longitude, positionCartographic.latitude, 10),
+name: ring2Radius_km.toFixed(1) + " km radius",
+description:'\
+<img\
+  width="50%"\
+  style="float:left; margin: 0 1em 1em 0;"\
+  src="assets/overpressure-distance.png"/>\
+<h3>\
+  Peak overpressure level:\
+</h3>\
+<p>\
+  68.94 kPa\
+</p>\
+<h3>\
+  Estimated Blast Structural Damage:\
+</h3>\
+<p>\
+  Reinforced concrete buildings are severely damaged or demolished.\
+</p>', // The Effects of Nuclear Weapons. (1977). United States: Department of Defense.
+ellipse: {
+    semiMinorAxis: ring2Radius_m,
+    semiMajorAxis: ring2Radius_m,
+    material: Cesium.Color.RED.withAlpha(0.2),
+},
+});
+
+ring3Radius_km = Math.pow(warheadYield,0.33)*0.57; // Blast Wave Effects Calculator by Jean M. Bele, Physics Dept., Laboratory for Nuclear Science, MIT https://nuclearweaponsedproj.mit.edu/Node/104
+// console.log(ring3Radius_km);
+ring3Radius_m = kmtom(ring3Radius_km);
+// console.log(ring3Radius_m)
+var ring3 = viewer.entities.add({
+position: Cesium.Cartesian3.fromRadians(positionCartographic.longitude, positionCartographic.latitude, 10),
+name: ring3Radius_km.toFixed(1) + " km radius",
+description:'\
+<img\
+  width="50%"\
+  style="float:left; margin: 0 1em 1em 0;"\
+  src="assets/overpressure-distance.png"/>\
+<h3>\
+  Peak overpressure level:\
+</h3>\
+<p>\
+  34.47 kPa\
+</p>\
+<h3>\
+  Estimated Blast Structural Damage:\
+</h3>\
+<p>\
+  Most buildings collapse.\
+</p>', // The Effects of Nuclear Weapons. (1977). United States: Department of Defense.
+ellipse: {
+    semiMinorAxis: ring3Radius_m,
+    semiMajorAxis: ring3Radius_m,
+    material: Cesium.Color.RED.withAlpha(0.2),
+},
+});
+
+var ring4Radius_km = Math.pow(warheadYield,0.33)*0.79; // Blast Wave Effects Calculator by Jean M. Bele, Physics Dept., Laboratory for Nuclear Science, MIT https://nuclearweaponsedproj.mit.edu/Node/104
+// console.log(ring4Radius_km);
+ring4Radius_m = kmtom(ring4Radius_km);
+// console.log(ring4Radius_m)
+var ring4 = viewer.entities.add({
+position: Cesium.Cartesian3.fromRadians(positionCartographic.longitude, positionCartographic.latitude, 10),
+name: ring4Radius_km.toFixed(1) + " km radius",
+description:'\
+<img\
+  width="50%"\
+  style="float:left; margin: 0 1em 1em 0;"\
+  src="assets/overpressure-distance.png"/>\
+<h3>\
+  Peak overpressure level:\
+</h3>\
+<p>\
+  20.68 kPa\
+</p>\
+<h3>\
+  Estimated Blast Structural Damage:\
+</h3>\
+<p>\
+  Residential structures collapse.\
+</p>', // The Effects of Nuclear Weapons. (1977). United States: Department of Defense.
+ellipse: {
+    semiMinorAxis: ring4Radius_m,
+    semiMajorAxis: ring4Radius_m,
+    material: Cesium.Color.RED.withAlpha(0.3),
+},
+});
+
+var ring5Radius_km = Math.pow(warheadYield,0.33)*1.66; // Blast Wave Effects Calculator by Jean M. Bele, Physics Dept., Laboratory for Nuclear Science, MIT https://nuclearweaponsedproj.mit.edu/Node/104
+// console.log(ring5Radius_km)
+ring5Radius_m = kmtom(ring5Radius_km);
+// console.log(ring5Radius_m)
+var ring5 = viewer.entities.add({
+position: Cesium.Cartesian3.fromRadians(positionCartographic.longitude, positionCartographic.latitude, 10),
+name: ring5Radius_km.toFixed(1) + " km radius",
+description:'\
+<img\
+  width="50%"\
+  style="float:left; margin: 0 1em 1em 0;"\
+  src="assets/overpressure-distance.png"/>\
+<h3>\
+  Peak overpressure level:\
+</h3>\
+<p>\
+  6.89 kPa\
+</p>\
+<h3>\
+  Estimated Blast Structural Damage:\
+</h3>\
+<p>\
+  Window glass shatters.\
+</p>', // The Effects of Nuclear Weapons. (1977). United States: Department of Defense.
+ellipse: {
+    semiMinorAxis: ring5Radius_m,
+    semiMajorAxis: ring5Radius_m,
+    material: Cesium.Color.YELLOW.withAlpha(0.2),
+},
+});
+fireballRadius = (Math.pow(warheadYield,0.4)*110)*0.3048; // Nuclear Fireball Calculator by Jean M. Bele, Physics Dept., Laboratory for Nuclear Science, MIT https://nuclearweaponsedproj.mit.edu/Node/105
+// console.log(fireballRadius)
+fireball_radiusFixed = fireballRadius.toFixed(1);
+// console.log(fireball_radiusFixed)
+var mcStem = viewer.entities.add({
+  name: "Fireball Radius, Mushroom Cloud Stem",
+  description:`\
+<img\
+  width="50%"\
+  style="float:left; margin: 0 1em 1em 0;"\
+  src="assets/stabilized-cloud-dimensions.png"/>\
+  <h3>\
+  Mushroom Cloud Altitude:\
+</h3>\
+<p>\
+  11.8 km\
+</p>\
+<h3>\
+  Fireball Radius:\
+</h3>\
+<p>\
+  ${fireball_radiusFixed} m\
+</p>\
+<h3>\
+  Peak overpressure level:\
+</h3>\
+<p>\
+  1378.95 kPa\
+</p>\
+<h3>\
+  <br>\
+  Estimated Blast Structural Damage:\
+</h3>\
+<p>\
+  Anything inside the fireball is effectively vaporized.\
+</p>`, // Nukemap by Alex Wellerstein https://nuclearsecrecy.com/nukemap/
+  position: Cesium.Cartesian3.fromRadians(positionCartographic.longitude, positionCartographic.latitude),
+  ellipse: {
+    semiMinorAxis: fireballRadius,
+    semiMajorAxis: fireballRadius,
+    extrudedHeight: 11800, // Nukemap by Alex Wellerstein https://nuclearsecrecy.com/nukemap/
+    material: Cesium.Color.RED.withAlpha(0.3),
+    outline: false,
+    heightReference: Cesium.HeightReference.RELATIVE_TO_GROUND
+  },
+});
+
+var mcHead = viewer.entities.add({
+  name: "Mushroom Cloud Head",
+  description:'\
+<img\
+  width="50%"\
+  style="float:left; margin: 0 1em 1em 0;"\
+  src="assets/stabilized-cloud-dimensions.png"/>\
+<h3>\
+  Mushroom Cloud Head Diameter:\
+</h3>\
+<p>\
+  10.04 km\
+</p>\
+<h3>\
+  Mushroom Cloud Head Height:\
+</h3>\
+<p>\
+  6.02 km\
+</p>', // Nukemap by Alex Wellerstein https://nuclearsecrecy.com/nukemap/
+  position: Cesium.Cartesian3.fromRadians(positionCartographic.longitude, positionCartographic.latitude, 11800), // Nukemap by Alex Wellerstein https://nuclearsecrecy.com/nukemap/
+  ellipsoid: {
+    radii: new Cesium.Cartesian3(10400, 10400, 6020), // Nukemap by Alex Wellerstein https://nuclearsecrecy.com/nukemap/
+    maximumCone: Cesium.Math.PI_OVER_TWO,
+    material: Cesium.Color.RED.withAlpha(0.3),
+    outline: false,
+    heightReference: Cesium.HeightReference.NONE
+  },
+});
+var lon = Cesium.Math.toDegrees(positionCartographic.longitude).toFixed(10);
+var lat = Cesium.Math.toDegrees(positionCartographic.latitude).toFixed(10);
+// console.log(lon);
+// console.log(lat);
+
+arcgisRest.queryDemographicData({
+  studyAreas: [{ "areaType":"RingBuffer","bufferUnits":"esriKilometers","bufferRadii":[3.6], "geometry": { "x": lon, "y": lat }}], // Graphs of Nuclear Weapons Effects by Dr. Wm. Robert Johnston http://www.johnstonsarchive.net/nuclear/nukgr3.pdf
+  authentication: authentication
+})
+.then((response) => {
+  // console.log("Demographics:", response);
+  try {
+    demographicsData = response["results"][0]['value']['FeatureSet'][0]['features'][0]['attributes'];
+  }
+  catch(err) {
+    document.getElementById("estimatedCasualties").textContent = "Cannot Estimate Casualties"
+    document.getElementById("totalPopulation").textContent = "No population data found"
+    createreloadButton();
+  }
+  totalPopulation = demographicsData.TOTPOP.toLocaleString()
+  document.getElementById("estimatedCasualties").textContent = "Estimated Casualties:"
+  document.getElementById("totalPopulation").textContent = totalPopulation + " people within 3.6 km radius dead"
+  var img = document.createElement("img");
+  img.src = "assets/yield-distance-effect.png"; // Graphs of Nuclear Weapons Effects by Dr. Wm. Robert Johnston http://www.johnstonsarchive.net/nuclear/nukgr3.pdf
+  img.className = "img"
+  var src = document.getElementById("image").appendChild(img);
+  createreloadButton();
+  // console.log(demographicsData);
+});
+
+const hypocenterMarker = viewer.entities.add({
+name : 'HYPOCENTER',
+position : Cesium.Cartesian3.fromRadians(positionCartographic.longitude, positionCartographic.latitude),
+point : {
+pixelSize : 5,
+color : Cesium.Color.RED,
+outlineColor : Cesium.Color.WHITE,
+outlineWidth : 2,
+heightReference: Cesium.HeightReference.RELATIVE_TO_GROUND 
+},
+label : {
+text : 'HYPOCENTER',
+font : '14pt monospace',
+style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+outlineWidth : 2,
+verticalOrigin : Cesium.VerticalOrigin.BOTTOM,
+pixelOffset : new Cesium.Cartesian2(0, -9),
+heightReference: Cesium.HeightReference.RELATIVE_TO_GROUND 
+}
+});
+
+const airburstMarker = viewer.entities.add({
+name : 'AIRBURST',
+position : Cesium.Cartesian3.fromRadians(positionCartographic.longitude, positionCartographic.latitude, detonationAltitude),
+point : {
+pixelSize : 5,
+color : Cesium.Color.RED,
+outlineColor : Cesium.Color.WHITE,
+outlineWidth : 2,
+heightReference: Cesium.HeightReference.RELATIVE_TO_GROUND 
+},
+label : {
+text : 'AIRBURST',
+font : '14pt monospace',
+style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+outlineWidth : 2,
+verticalOrigin : Cesium.VerticalOrigin.BOTTOM,
+pixelOffset : new Cesium.Cartesian2(0, -9),
+heightReference: Cesium.HeightReference.RELATIVE_TO_GROUND
+}
+});
+}
+
+// Start off looking at Beijing.
+viewer.scene.camera.flyTo({
+destination: Cesium.Cartesian3.fromDegrees(116.383331, 39.916668, 700)
+}); 
